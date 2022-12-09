da4563d252497f5fe7a7e9e6ebb8accc785328a7
HenryGrammer <schultzxhenry@gmail.com>
2022-12-08 20:24:19 -0800

    updated

--- teachers/class-list.php
+++ teachers/class-list.php
@@ -5,8 +5,9 @@
   $id = $_SESSION['id'];
   $subjects = $_SESSION['subjects'];
   $sections = $_SESSION['sections'];
-  $query = "SELECT * FROM class_list WHERE users_id = '$id' AND subjects = '$subjects' AND sections = '$sections'";
-  $sql = mysqli_query($con, $query);
+  
+  // $query = "SELECT * FROM class_list WHERE users_id = '$id' AND subjects = '$subjects' AND sections = '$sections'";
+  // $sql = mysqli_query($con, $query);
 
 ?>
 
@@ -90,7 +91,7 @@
 
           <tbody>
             <?php 
-              $result = $con->query("SELECT * FROM class_list WHERE users_id = '$id' AND subject = '$subjects' AND section = '$sections'");
+              $result = $con->query("SELECT * FROM class_list");
 
               if ($result->num_rows > 0) {
                 while($row = $result->fetch_array()){
@@ -121,7 +122,7 @@
               } else {
                 ?>
             <tr>
-              <td colspan="6">No Class List Found...</td>
+              <td colspan="8">No Class List Found...</td>
             </tr>
             <?php
               }
